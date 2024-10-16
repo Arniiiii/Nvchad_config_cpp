ed37809a4542463b2153993ec40f40e3bf7b257f
Arniiiii <lg3dx6fd@gmail.com>
2024-10-16 19:35:20 +0000

    temporary experiment with jukit

---
+++ lua/configs/vim-jukit.lua
@@ -0,0 +1,26 @@
+vim.cmd([[
+let g:jukit_shell_cmd = 'ipython3'
+"    - Specifies the command used to start a shell in the output split. Can also be an absolute path. Can also be any other shell command, e.g. `R`, `julia`, etc. (note that output saving is only possible for ipython)
+let g:jukit_terminal = 'kitty'
+"   - Terminal to use. Can be one of '', 'kitty', 'vimterm', 'nvimterm' or 'tmux'. If '' is given then will try to detect terminal (though this might fail, in which case it simply defaults to 'vimterm' or 'nvimterm' - depending on the output of `has("nvim")`)
+let g:jukit_auto_output_hist = 0
+"   - If set to 1, will create an autocmd with event `CursorHold` to show saved ipython output of current cell in output-history split. Might slow down (n)vim significantly, you can use `set updatetime=<number of milliseconds>` to control the time to wait until CursorHold events are triggered, which might improve performance if set to a higher number (e.g. `set updatetime=1000`).
+let g:jukit_use_tcomment = 0
+"   - Whether to use tcomment plugin (https://github.com/tomtom/tcomment_vim) to comment out cell markers. If not, then cell markers will simply be prepended with `g:jukit_comment_mark`
+let g:jukit_comment_mark = '#'
+"   - See description of `g:jukit_use_tcomment` above
+let g:jukit_mappings = 1
+"   - If set to 0, none of the default function mappings (as specified further down) will be applied
+let g:jukit_mappings_ext_enabled = "*"
+"   - String or list of strings specifying extensions for which the mappings will be created. For example, `let g:jukit_mappings_ext_enabled=['py', 'ipynb']` will enable the mappings only in `.py` and `.ipynb` files. Use `let g:jukit_mappings_ext_enabled='*'` to enable them for all files.
+let g:jukit_notebook_viewer = 'jupyter-notebook'
+"   - Command to open .ipynb files, by default jupyter-notebook is used. To use e.g. vs code instead, you could set this to `let g:jukit_notebook_viewer = 'code'`
+let g:jukit_convert_overwrite_default = -1
+"   - Default setting when converting from .ipynb to .py or vice versa and a file of the same name already exists. Can be of [-1, 0, 1], where -1 means no default (i.e. you'll be prompted to specify what to do), 0 means never overwrite, 1 means always overwrite
+let g:jukit_convert_open_default = -1
+"   - Default setting for whether the notebook should be opened after converting from .py to .ipynb. Can be of [-1, 0, 1], where -1 means no default (i.e. you'll be prompted to specify what to do), 0 means never open, 1 means always open
+let g:jukit_file_encodings = 'utf-8'
+"   - Default encoding for reading and writing to files in the python helper functions
+let g:jukit_venv_in_output_hist = 1
+"   - Whether to also use the provided terminal command for the output history split when starting the splits using the JukitOUtHist command. If 0, the provided terminal command is only used in the output split, not in the output history split.
+]])
