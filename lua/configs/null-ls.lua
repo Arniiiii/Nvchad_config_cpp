ce11371af2a85be1bc61bef964b8847d678fff1f
Arniiiii <lg3dx6fd@gmail.com>
2024-11-26 15:01:02 +0000

    something...

--- lua/configs/null-ls.lua
+++ lua/configs/null-ls.lua
@@ -5,7 +5,7 @@ local b = null_ls.builtins
 local sources = {
 
   -- webdev stuff
-  b.formatting.deno_fmt, -- choosed deno for ts/js files cuz its very fast!
+  -- b.formatting.deno_fmt, -- choosed deno for ts/js files cuz its very fast!
   b.formatting.prettier.with { filetypes = { "html", "markdown", "css" } }, -- so prettier works only on these filetypes
 
   -- Lua
@@ -20,6 +20,9 @@ local sources = {
   -- cmake
   b.diagnostics.cmake_lint,
   b.formatting.cmake_format,
+
+  -- java
+  b.formatting.tidy,
 }
 
 null_ls.setup {
